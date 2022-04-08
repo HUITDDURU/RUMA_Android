40955459063d3c1143cdee8e8c2b75c233dc8b29
unknown <abh0920one@gmail.com>
2022-04-08 12:05:56 +0900

    ➕️ (#2) - Splash screen layout 구성

--- presentation/src/main/java/com/example/huitdduru/view/splash/SplashActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/splash/SplashActivity.kt
@@ -1,12 +1,12 @@
-package com.example.huitdduru.view
+package com.example.huitdduru.view.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.huitdduru.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
