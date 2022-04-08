73c64c3f76f59be02e54495cb280d6e40037e71f
unknown <abh0920one@gmail.com>
2022-04-08 11:37:46 +0900

    ⚙️ - 패키지 구조 세팅

--- presentation/src/main/java/com/example/huitdduru/view/MainActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.huitdduru
+package com.example.huitdduru.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.huitdduru.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
