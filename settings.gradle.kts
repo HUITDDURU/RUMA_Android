e6f8ca5b4a68a0e743c0e66ea576453857c75492
unknown <abh0920one@gmail.com>
2022-04-14 19:32:38 +0900

    ⚙️ (#3) - setting.gradle kts migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "HUITDDURU"
-include ':presentation'
-include ':data'
-include ':domain'
+include(":presentation")
+include(":data")
+include(":domain")
