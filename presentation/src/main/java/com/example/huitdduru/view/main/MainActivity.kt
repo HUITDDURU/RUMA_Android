3cd805eb9add65faba2b080fa8494cf05efa37df
unknown <abh0920one@gmail.com>
2022-04-20 23:09:53 +0900

    🔧 (#6) - MainActivity base activity 적용

--- presentation/src/main/java/com/example/huitdduru/view/main/MainActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/main/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.huitdduru.view.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.huitdduru.R
+import com.example.huitdduru.databinding.ActivityMainBinding
+import com.example.huitdduru.view.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
