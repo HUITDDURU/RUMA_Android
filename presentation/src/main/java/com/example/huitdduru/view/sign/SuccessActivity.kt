a4e6d8de41f4033a2b33546c9604ba02a52cf3df
unknown <abh0920one@gmail.com>
2022-04-21 08:52:56 +0900

    🔧 (#6) - success activity base activity 적용

--- presentation/src/main/java/com/example/huitdduru/view/sign/SuccessActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/SuccessActivity.kt
@@ -3,10 +3,11 @@ package com.example.huitdduru.view.sign
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.huitdduru.R
+import com.example.huitdduru.databinding.ActivitySuccessBinding
+import com.example.huitdduru.view.base.BaseActivity
 
-class SuccessActivity : AppCompatActivity() {
+class SuccessActivity : BaseActivity<ActivitySuccessBinding>(R.layout.activity_success) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_success)
     }
 }
