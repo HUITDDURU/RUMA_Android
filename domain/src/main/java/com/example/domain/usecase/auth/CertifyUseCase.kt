a8a159d38b2d56f4911b2e8c7582f1807e8b9331
unknown <abh0920one@gmail.com>
2022-04-26 10:03:50 +0900

    ➕️ (#14) - 인증번호 확인 usecase 추가

---
+++ domain/src/main/java/com/example/domain/usecase/auth/CertifyUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.domain.usecase.auth
+
+import com.example.domain.entity.auth.CertifyRequest
+import com.example.domain.repository.AuthRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class CertifyUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend fun invoke(data: CertifyRequest) : Response<Unit> =
+        authRepository.certify(data)
+}
