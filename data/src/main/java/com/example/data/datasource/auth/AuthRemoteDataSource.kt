6c2a9baa6e776badf74d5c49e65affea70d4f143
unknown <abh0920one@gmail.com>
2022-04-26 23:25:53 +0900

    ➕️ (#14) - auth data source

---
+++ data/src/main/java/com/example/data/datasource/auth/AuthRemoteDataSource.kt
@@ -0,0 +1,17 @@
+package com.example.data.datasource.auth
+
+import com.example.data.remote.request.CertifyRequest
+import com.example.data.remote.request.LoginRequest
+import com.example.data.remote.request.RegisterRequest
+import com.example.data.remote.request.SendCodeRequest
+import com.example.data.remote.response.LoginResponse
+import com.example.data.remote.response.TokenRefreshResponse
+import com.example.domain.util.RemoteErrorEmitter
+
+interface AuthRemoteDataSource {
+    suspend fun login(remoteErrorEmitter: RemoteErrorEmitter ,body: LoginRequest) : LoginResponse?
+    suspend fun register(remoteErrorEmitter: RemoteErrorEmitter ,part: RegisterRequest) : Unit
+    suspend fun sendCode(remoteErrorEmitter: RemoteErrorEmitter ,body: SendCodeRequest) : Unit
+    suspend fun certify(remoteErrorEmitter: RemoteErrorEmitter ,body: CertifyRequest) : Unit
+    suspend fun tokenRefresh(remoteErrorEmitter: RemoteErrorEmitter ,header: String) : TokenRefreshResponse
+}
