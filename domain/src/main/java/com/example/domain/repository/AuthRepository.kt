0066ec01f5e26159f225d923001496f28cf00ea6
unknown <abh0920one@gmail.com>
2022-05-08 01:58:35 +0900

    🔧 (#14) - auth repository remoteErrorEmitter parameter add

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,12 +1,13 @@
 package com.example.domain.repository
 
 import com.example.domain.entity.auth.*
+import com.example.domain.util.RemoteErrorEmitter
 import retrofit2.Response
 
 interface AuthRepository {
-    suspend fun login(body: LoginRequestEntity): Response<LoginResponseEntity>
-    suspend fun register(part: RegisterRequestEntity): Response<Unit>
-    suspend fun sendCode(body: SendCodeRequestEntity): Response<Unit>
-    suspend fun certify(body: CertifyRequestEntity): Response<Unit>
-    suspend fun tokenRefresh(header: String): Response<TokenRefreshResponseEntity>
+    suspend fun login(remoteErrorEmitter: RemoteErrorEmitter, body: LoginRequestEntity): Response<LoginResponseEntity>
+    suspend fun register(remoteErrorEmitter: RemoteErrorEmitter, part: RegisterRequestEntity): Response<Unit>
+    suspend fun sendCode(remoteErrorEmitter: RemoteErrorEmitter, body: SendCodeRequestEntity): Response<Unit>
+    suspend fun certify(remoteErrorEmitter: RemoteErrorEmitter, body: CertifyRequestEntity): Response<Unit>
+    suspend fun tokenRefresh(remoteErrorEmitter: RemoteErrorEmitter, header: String): Response<TokenRefreshResponseEntity>
 }
