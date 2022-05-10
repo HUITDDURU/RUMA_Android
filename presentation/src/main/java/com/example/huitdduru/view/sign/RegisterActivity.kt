4a58cbaeea4a46e025e4f9887da6ca3158c531e7
unknown <abh0920one@gmail.com>
2022-05-10 22:38:33 +0900

    🔌 (#14) - fragment add

--- presentation/src/main/java/com/example/huitdduru/view/sign/RegisterActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/RegisterActivity.kt
@@ -1,14 +1,23 @@
 package com.example.huitdduru.view.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.ActivityRegisterBinding
 import com.example.huitdduru.view.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity_register) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.activity = this
 
+        val fragmentManager = supportFragmentManager
+        fragmentManager.commit {
+            add(R.id.container, InfoFragment())
+            add(R.id.container, CertifyFragment())
+            add(R.id.container, UserFragment())
+        }
     }
 }
