8655b2d2e5dac5ec790e1df0fa43cf560b71bc9b
unknown <abh0920one@gmail.com>
2022-05-10 22:37:29 +0900

    🔌 (#14) - 이메일 인증 코드 발송

--- presentation/src/main/java/com/example/huitdduru/view/sign/InfoFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/InfoFragment.kt
@@ -1,17 +1,57 @@
 package com.example.huitdduru.view.sign
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.FragmentInfoBinding
+import com.example.huitdduru.util.ToastType
+import com.example.huitdduru.util.repeatOnStarted
 import com.example.huitdduru.view.base.BaseFragment
+import com.example.huitdduru.viewmodel.register.RegisterViewModel
+import com.example.huitdduru.viewmodel.register.RegisterViewModel.Event
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class InfoFragment : BaseFragment<FragmentInfoBinding>(R.layout.fragment_info) {
+
+    private val vm: RegisterViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fragment = this
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    fun sendCode() {
+        if(binding.emailEt.text.toString() == "" ||
+            binding.passwordEt.text.toString() == "" ||
+            binding.confirmEt.text.toString() == ""){
+            showToast("정보를 입력해주세요.", ToastType.INFO)
+        } else {
+            passwordConfirm()
+        }
+    }
+
+    private fun passwordConfirm() {
+        if(binding.passwordEt.text.toString() !=
+                binding.confirmEt.text.toString()){
+            showToast("비밀번호가 일치하지 않습니다.", ToastType.ERROR)
+        } else {
+            vm.sendCode(binding.emailEt.text.toString())
+        }
+    }
+
+    private fun handleEvent(event: Event) = when(event) {
+        is Event.SuccessEmailSend -> {
+            showToast("인증코드가 발송되었습니다!", ToastType.SUCCESS)
+            vm.email = binding.emailEt.text.toString()
+            vm.password = binding.passwordEt.text.toString()
+        }
+        else -> {}
     }
 }
