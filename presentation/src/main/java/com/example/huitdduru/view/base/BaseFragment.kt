032bea0503cbf824a2165522f9b583fa1c433786
unknown <abh0920one@gmail.com>
2022-05-10 19:42:01 +0900

    🔌 (#14) - base fragment show toasty function add

--- presentation/src/main/java/com/example/huitdduru/view/base/BaseFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/base/BaseFragment.kt
@@ -1,13 +1,17 @@
 package com.example.huitdduru.view.base
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.example.huitdduru.util.ToastType
+import es.dmoral.toasty.Toasty
 
 abstract class BaseFragment<T: ViewDataBinding>(@LayoutRes private val layoutResId: Int) : Fragment(){
     private var _binding: T? = null
@@ -26,5 +30,15 @@ abstract class BaseFragment<T: ViewDataBinding>(@LayoutRes private val layoutRes
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @SuppressLint("CheckResult")
+    fun showToast(msg: String, type: ToastType){
+        when(type){
+            ToastType.ERROR -> Toasty.error(requireContext(), msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.SUCCESS -> Toasty.success(requireContext(), msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.INFO -> Toasty.info(requireContext(), msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.WARNING -> Toasty.warning(requireContext(), msg, Toast.LENGTH_SHORT, true).show()
+        }
     }
 }
