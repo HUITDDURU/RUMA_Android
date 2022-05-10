0d84b2c6fef3253ba534e8c513193629f042b673
unknown <abh0920one@gmail.com>
2022-05-10 19:38:28 +0900

    🔌 (#14) - showToasty function add

--- presentation/src/main/java/com/example/huitdduru/view/base/BaseActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/base/BaseActivity.kt
@@ -1,10 +1,14 @@
 package com.example.huitdduru.view.base
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.huitdduru.util.ToastType
+import es.dmoral.toasty.Toasty
 
 abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
     lateinit var binding : T
@@ -13,5 +17,15 @@ abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes private val layoutRes
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
         binding.lifecycleOwner = this
+    }
+
+    @SuppressLint("CheckResult")
+    fun showToast(msg: String, type: ToastType){
+        when(type){
+            ToastType.ERROR -> Toasty.error(this, msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.SUCCESS -> Toasty.success(this, msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.INFO -> Toasty.info(this, msg, Toast.LENGTH_SHORT, true).show()
+            ToastType.WARNING -> Toasty.warning(this, msg, Toast.LENGTH_SHORT, true).show()
+        }
     }
 }
