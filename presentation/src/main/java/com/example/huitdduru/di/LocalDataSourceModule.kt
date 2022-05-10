8c246a6985365ce1a1a975045423802c40601868
unknown <abh0920one@gmail.com>
2022-05-10 15:29:05 +0900

    ➕️ (#14) - local data source module add

---
+++ presentation/src/main/java/com/example/huitdduru/di/LocalDataSourceModule.kt
@@ -0,0 +1,20 @@
+package com.example.huitdduru.di
+
+import com.example.data.datasource.auth.LocalAuthDataSource
+import com.example.data.datasource.auth.LocalAuthDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocalDataSourceModule {
+
+    @Singleton
+    @Binds
+    abstract fun provideLocalAuthDataSource(
+        localAuthDataSourceImpl: LocalAuthDataSourceImpl
+    ): LocalAuthDataSource
+}
