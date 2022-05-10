382d3fcf85411c63742e3398df447d8fec8a2502
unknown <abh0920one@gmail.com>
2022-05-10 15:10:06 +0900

    🔌 (#14) - token set/get

--- data/src/main/java/com/example/data/datasource/auth/LocalAuthDataSource.kt
+++ data/src/main/java/com/example/data/datasource/auth/LocalAuthDataSource.kt
@@ -1,4 +1,15 @@
 package com.example.data.datasource.auth
 
+import kotlinx.coroutines.flow.Flow
+
 interface LocalAuthDataSource {
+
+    suspend fun setAccessToken(accessToken: String)
+    suspend fun getAccessToken(): Flow<String>
+    suspend fun resetAccessToken()
+
+    suspend fun setRefreshToken(refreshToken: String)
+    suspend fun getRefreshToken(): Flow<String>
+    suspend fun resetRefreshToken()
+
 }
--- data/src/main/java/com/example/data/datasource/auth/LocalAuthDataSourceImpl.kt
+++ data/src/main/java/com/example/data/datasource/auth/LocalAuthDataSourceImpl.kt
@@ -1,4 +1,31 @@
 package com.example.data.datasource.auth
 
-class LocalAuthDataSourceImpl {
+import com.example.data.local.storage.LocalDataStorage
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalAuthDataSourceImpl @Inject constructor(
+    private val localDataStorage: LocalDataStorage
+) : LocalAuthDataSource {
+    override suspend fun setAccessToken(accessToken: String) {
+        localDataStorage.setAccessToken(accessToken)
+    }
+
+    override suspend fun getAccessToken(): Flow<String> =
+        localDataStorage.getAccessToken()
+
+    override suspend fun resetAccessToken() {
+        localDataStorage.setAccessToken("")
+    }
+
+    override suspend fun setRefreshToken(refreshToken: String) {
+        localDataStorage.setRefreshToken(refreshToken)
+    }
+
+    override suspend fun getRefreshToken(): Flow<String> =
+        localDataStorage.getRefreshToken()
+
+    override suspend fun resetRefreshToken() {
+        localDataStorage.setRefreshToken("")
+    }
 }
