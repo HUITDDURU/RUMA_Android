ee220a628bd1d1e0f18f7e1b376460527bd9d777
unknown <abh0920one@gmail.com>
2022-05-10 17:18:24 +0900

    🔌 (#14) - 회원가입 페이지 이동

--- presentation/src/main/java/com/example/huitdduru/view/sign/LoginActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.huitdduru.view.sign
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.ActivityLoginBinding
@@ -8,5 +9,12 @@ import com.example.huitdduru.view.base.BaseActivity
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.activity = this
+
+    }
+
+    fun moveToRegister() {
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
