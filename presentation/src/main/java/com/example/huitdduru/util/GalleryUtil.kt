8757244ed3d9816ac486691bbaff6b90496b1770
unknown <abh0920one@gmail.com>
2022-05-11 17:07:36 +0900

    🔌 (#14) - uri to multipart function add

--- presentation/src/main/java/com/example/huitdduru/util/GalleryUtil.kt
+++ presentation/src/main/java/com/example/huitdduru/util/GalleryUtil.kt
@@ -2,12 +2,23 @@ package com.example.huitdduru.util
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.provider.MediaStore
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
+import java.lang.Exception
 
 class GalleryUtil(private val activity: Activity) {
     fun openGallery(startForResult: ActivityResultLauncher<Intent>) {
@@ -37,5 +48,22 @@ class GalleryUtil(private val activity: Activity) {
             }
             startForResult.launch(intent)
         }
+    }
+
+    fun uriToMultipart(context: Context ,filePath: String, uri: Uri) : MultipartBody.Part {
+        val file = File(filePath)
+        var inputStream: InputStream? = null
+        try{
+            inputStream = context.contentResolver.openInputStream(uri)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream)
+        val requestFile = byteArrayOutputStream.toByteArray().toRequestBody("image/jpg".toMediaTypeOrNull())
+        val body = MultipartBody.Part.createFormData("file", file.name, requestFile)
+        return body
     }
 }
