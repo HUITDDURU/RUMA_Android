8dfcc57168a04bcdd43531e719f5cace6b44a36c
unknown <abh0920one@gmail.com>
2022-05-12 08:52:12 +0900

    🔌 (#14) - user fragment event handling

--- presentation/src/main/java/com/example/huitdduru/view/sign/UserFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/UserFragment.kt
@@ -1,20 +1,45 @@
 package com.example.huitdduru.view.sign
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.FragmentUserBinding
 import com.example.huitdduru.util.GalleryUtil
+import com.example.huitdduru.util.ToastType
+import com.example.huitdduru.util.repeatOnStarted
 import com.example.huitdduru.view.base.BaseFragment
+import com.example.huitdduru.viewmodel.register.RegisterViewModel
+import com.example.huitdduru.viewmodel.register.RegisterViewModel.Event
+import kotlinx.coroutines.flow.collect
 
 class UserFragment : BaseFragment<FragmentUserBinding>(R.layout.fragment_user) {
+
+    private val vm by activityViewModels<RegisterViewModel>()
+    private lateinit var galleryUtil: GalleryUtil
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
+        galleryUtil = GalleryUtil(requireActivity())
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: Event) = when(event) {
+        is Event.SuccessRegister -> {
+            val intent = Intent(requireContext(), SuccessActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+        else -> {}
     }
 
     private val profile =
@@ -22,10 +47,25 @@ class UserFragment : BaseFragment<FragmentUserBinding>(R.layout.fragment_user) {
             if(it.resultCode == Activity.RESULT_OK){
                 binding.profileIv.clipToOutline = true
                 Glide.with(requireActivity())
-                    .load(it.data?.data).into(binding.profileIv)}
+                    .load(it.data?.data)
+                    .into(binding.profileIv)
+                vm.file = galleryUtil.uriToMultipart(
+                    requireContext(),
+                    it.data?.data!!.path!!,
+                    it.data?.data!!
+                )
+            }
         }
 
-    fun openGallery(){
-        GalleryUtil(requireActivity()).openGallery(profile)
+    fun openGallery() = galleryUtil.openGallery(profile)
+
+    fun register() {
+        if(binding.nicknameEt.text.toString() == ""){
+            showToast("닉네임을 입력해주세요!", ToastType.INFO)
+        } else {
+            vm.name = binding.nicknameEt.text.toString().trim()
+            vm.intro = binding.descriptionEt.text.toString().trim()
+            vm.register()
+        }
     }
 }
