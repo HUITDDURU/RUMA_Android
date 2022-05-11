2bf19106547dc9f92f3122693291688ad9f8095e
unknown <abh0920one@gmail.com>
2022-05-11 15:10:55 +0900

    🔌 (#14) - 갤러리에서 사진 가져오기

--- presentation/src/main/java/com/example/huitdduru/view/sign/UserFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/UserFragment.kt
@@ -1,17 +1,31 @@
 package com.example.huitdduru.view.sign
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.FragmentUserBinding
+import com.example.huitdduru.util.GalleryUtil
 import com.example.huitdduru.view.base.BaseFragment
 
 class UserFragment : BaseFragment<FragmentUserBinding>(R.layout.fragment_user) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
+    }
+
+    private val profile =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == Activity.RESULT_OK){
+                binding.profileIv.clipToOutline = true
+                Glide.with(requireActivity())
+                    .load(it.data?.data).into(binding.profileIv)}
+        }
+
+    fun openGallery(){
+        GalleryUtil(requireActivity()).openGallery(profile)
     }
 }
