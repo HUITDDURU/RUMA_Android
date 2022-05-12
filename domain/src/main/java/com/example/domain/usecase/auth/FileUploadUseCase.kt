29766ee5650ba4010a84912fb7003153a8eb63db
unknown <abh0920one@gmail.com>
2022-05-12 22:26:37 +0900

    ➕️ (#14) - file upload usecase add

--- domain/src/main/java/com/example/domain/usecase/auth/FileUploadUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/FileUploadUseCase.kt
@@ -1,4 +1,12 @@
 package com.example.domain.usecase.auth
 
-class FileUploadUseCase {
+import com.example.domain.repository.AuthRepository
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class FileUploadUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+){
+    suspend fun invoke(file: MultipartBody.Part) =
+        authRepository.fileUpload(file)
 }
