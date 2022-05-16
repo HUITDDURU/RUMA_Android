e76f3658cc45204e79ab3d81e9bacbeb1de24efe
unknown <abh0920one@gmail.com>
2022-05-16 20:02:42 +0900

    ➕️ (#17) - write activity add

---
+++ presentation/src/main/java/com/example/huitdduru/view/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.huitdduru.view.write
+
+import android.os.Bundle
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseActivity
+import com.example.huitdduru.databinding.ActivityWriteBinding
+
+class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
