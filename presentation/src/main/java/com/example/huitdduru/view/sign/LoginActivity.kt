e080bf13bac895ad0e253edeafa4a66dba9e38ce
unknown <abh0920one@gmail.com>
2022-05-16 18:55:42 +0900

    🔌 (#14) - login activity event handler add

--- presentation/src/main/java/com/example/huitdduru/view/sign/LoginActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/sign/LoginActivity.kt
@@ -2,19 +2,52 @@ package com.example.huitdduru.view.sign
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.ActivityLoginBinding
 import com.example.huitdduru.base.BaseActivity
+import com.example.huitdduru.util.ToastType
+import com.example.huitdduru.util.repeatOnStarted
+import com.example.huitdduru.view.main.MainActivity
+import com.example.huitdduru.viewmodel.login.LoginViewModel
+import com.example.huitdduru.viewmodel.login.LoginViewModel.Event
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    private val vm : LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.activity = this
 
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handlerEvent(event) }
+        }
+    }
+
+    fun login() {
+        val email = binding.emailEt.text.toString().trim()
+        val password = binding.passwordEt.text.toString().trim()
+        if(email == "" || password == ""){
+            showToast("이메일 및 패스워드를 입력해주세요.", ToastType.INFO)
+        } else {
+            vm.login(email, password)
+        }
     }
 
     fun moveToRegister() {
         val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun handlerEvent(event: Event) = when(event){
+        is Event.SuccessLogin -> {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+        is Event.ErrorMessage -> { showToast(event.errorMessage, ToastType.ERROR) }
     }
 }
