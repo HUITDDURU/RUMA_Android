e06a3c7f7ff6abb869d7fd05b8f52a3e558cc616
unknown <abh0920one@gmail.com>
2022-05-16 20:00:08 +0900

    ➕️ (#17) - exchange fragment add

---
+++ presentation/src/main/java/com/example/huitdduru/view/exchange/ExchangeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.huitdduru.view.exchange
+
+import android.os.Bundle
+import android.view.View
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseFragment
+import com.example.huitdduru.databinding.FragmentExchangeBinding
+
+class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(R.layout.fragment_exchange) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
