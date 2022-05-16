5251b8ad1c6df2232526a495e5da81d97462ba7d
unknown <abh0920one@gmail.com>
2022-05-16 20:05:46 +0900

    ➕️ (#17) - my page fragment add

---
+++ presentation/src/main/java/com/example/huitdduru/view/mypage/MyPageFragment.kt
@@ -0,0 +1,13 @@
+package com.example.huitdduru.view.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseFragment
+import com.example.huitdduru.databinding.FragmentMyPageBinding
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
