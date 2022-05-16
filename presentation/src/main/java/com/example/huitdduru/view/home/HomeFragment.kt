5f2fc1259b8d66f10acd8f334e653729531ef3f1
unknown <abh0920one@gmail.com>
2022-05-16 19:58:15 +0900

    ➕️ (#17) - home fragment add

---
+++ presentation/src/main/java/com/example/huitdduru/view/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.huitdduru.view.home
+
+import android.os.Bundle
+import android.view.View
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseFragment
+import com.example.huitdduru.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
