7b85e533d08d65f1f9cd4fda62dd7d673c2b83be
unknown <abh0920one@gmail.com>
2022-05-16 16:43:37 +0900

    🔌 (#14) - LoginViewModel login api

--- presentation/src/main/java/com/example/huitdduru/viewmodel/login/LoginViewModel.kt
+++ presentation/src/main/java/com/example/huitdduru/viewmodel/login/LoginViewModel.kt
@@ -2,6 +2,9 @@ package com.example.huitdduru.viewmodel.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.base.BadRequest
+import com.example.domain.base.NotFound
+import com.example.domain.entity.auth.LoginRequestEntity
 import com.example.domain.usecase.auth.LoginUseCase
 import com.example.huitdduru.util.MutableEventFlow
 import com.example.huitdduru.util.asEventFlow
@@ -20,6 +23,26 @@ class LoginViewModel @Inject constructor(
     private fun event(event: Event) {
         viewModelScope.launch {
             _eventFlow.emit(event)
+        }
+    }
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                loginUseCase.invoke(
+                    LoginRequestEntity(
+                        email,
+                        password
+                    )
+                )
+            }.onSuccess {
+                event(Event.SuccessLogin(true))
+            }.onFailure {
+                when(it){
+                    is BadRequest -> { event(Event.ErrorMessage("잘못된 요청 형식입니다.")) }
+                    is NotFound -> { event(Event.ErrorMessage("잘못된 요청입니다.")) }
+                }
+            }
         }
     }
 
