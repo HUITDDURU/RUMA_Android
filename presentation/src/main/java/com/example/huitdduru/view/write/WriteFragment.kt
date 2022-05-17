ea74c47d190e971f011e77b47ab7be7815c1fd9b
unknown <abh0920one@gmail.com>
2022-05-17 19:06:55 +0900

    😎 (#17) - write activity fragment로 변경

---
+++ presentation/src/main/java/com/example/huitdduru/view/write/WriteFragment.kt
@@ -0,0 +1,13 @@
+package com.example.huitdduru.view.write
+
+import android.os.Bundle
+import android.view.View
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseFragment
+import com.example.huitdduru.databinding.FragmentWriteBinding
+
+class WriteFragment : BaseFragment<FragmentWriteBinding>(R.layout.fragment_write) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
