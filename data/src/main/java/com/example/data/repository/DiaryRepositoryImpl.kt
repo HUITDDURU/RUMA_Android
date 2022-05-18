21fdba26ef12e12857fe2245d4fe3af5d673c21b
unknown <abh0920one@gmail.com>
2022-05-18 22:15:16 +0900

    ➕️ (#21) - diary repository implements

---
+++ data/src/main/java/com/example/data/repository/DiaryRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.data.repository
+
+import com.example.data.datasource.DiaryDataSource
+import com.example.data.mapper.DiaryMapper.mapperToDiaryTimeLine
+import com.example.data.mapper.DiaryMapper.mapperToGetDiaryList
+import com.example.data.mapper.DiaryMapper.mapperToWriteDiaryEntity
+import com.example.domain.entity.diary.DiaryResponseEntity
+import com.example.domain.entity.diary.DiaryTimeLineResponseEntity
+import com.example.domain.entity.diary.WriteDiaryRequestEntity
+import com.example.domain.repository.DiaryRepository
+import javax.inject.Inject
+
+class DiaryRepositoryImpl @Inject constructor(
+    private val diaryDataSource: DiaryDataSource
+): DiaryRepository {
+
+    override suspend fun writeDiary(header: String, diaryId: Int, body: WriteDiaryRequestEntity) {
+        diaryDataSource.writeDiary(header, diaryId, mapperToWriteDiaryEntity(body))
+    }
+
+    override suspend fun getDiaryList(header: String, diaryId: Int): List<DiaryResponseEntity> =
+        mapperToGetDiaryList(diaryDataSource.getDiaryList(header, diaryId))
+
+    override suspend fun diaryTimeLine(header: String): DiaryTimeLineResponseEntity =
+        mapperToDiaryTimeLine(diaryDataSource.diaryTimeLine(header))
+}
