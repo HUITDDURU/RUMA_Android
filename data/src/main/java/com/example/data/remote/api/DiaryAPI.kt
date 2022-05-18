6a81cfcbeea83319d2a9092b166297c982539c3b
unknown <abh0920one@gmail.com>
2022-05-18 17:10:15 +0900

    ➕️ (#21) - diary api add

---
+++ data/src/main/java/com/example/data/remote/api/DiaryAPI.kt
@@ -0,0 +1,27 @@
+package com.example.data.remote.api
+
+import com.example.data.remote.request.WriteDiaryRequest
+import com.example.data.remote.response.DiaryTimeLineResponse
+import com.example.data.remote.response.GetDiaryListResponse
+import retrofit2.http.*
+
+interface DiaryAPI {
+
+    @POST("/diary/{diaryId}")
+    suspend fun writeDiary(
+        @Header("Authorization") header: String,
+        @Path("diaryId") diaryId: Int,
+        @Body body: WriteDiaryRequest
+    )
+
+    @GET("/diary/{diaryId}/list")
+    suspend fun getDiaryList(
+        @Header("Authorization") header: String,
+        @Path("diaryId") diaryId: Int
+    ) : GetDiaryListResponse
+
+    @GET("/diary/chronology")
+    suspend fun diaryTimeLine(
+        @Header("Authorization") header: String
+    ) : DiaryTimeLineResponse
+}
