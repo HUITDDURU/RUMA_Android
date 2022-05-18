cca4f84238aefe5f2b451b2c3a432dfffbe9714f
unknown <abh0920one@gmail.com>
2022-05-18 22:11:49 +0900

    😎 (#21) - diary datasource response type fix

--- data/src/main/java/com/example/data/datasource/DiaryDataSource.kt
+++ data/src/main/java/com/example/data/datasource/DiaryDataSource.kt
@@ -1,11 +1,11 @@
 package com.example.data.datasource
 
 import com.example.data.remote.request.WriteDiaryRequest
+import com.example.data.remote.response.DiaryResponse
 import com.example.data.remote.response.DiaryTimeLineResponse
-import com.example.data.remote.response.GetDiaryListResponse
 
 interface DiaryDataSource {
     suspend fun writeDiary(header: String, diaryId: Int, body: WriteDiaryRequest)
-    suspend fun getDiaryList(header: String, diaryId: Int) : GetDiaryListResponse
+    suspend fun getDiaryList(header: String, diaryId: Int) : List<DiaryResponse>
     suspend fun diaryTimeLine(header: String) : DiaryTimeLineResponse
 }
