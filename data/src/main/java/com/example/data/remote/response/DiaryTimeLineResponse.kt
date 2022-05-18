4efd082698d866db85effa59bfaa86412414674c
unknown <abh0920one@gmail.com>
2022-05-18 22:13:34 +0900

    😎 (#21) - diary time line response list type refactor

--- data/src/main/java/com/example/data/remote/response/DiaryTimeLineResponse.kt
+++ data/src/main/java/com/example/data/remote/response/DiaryTimeLineResponse.kt
@@ -2,7 +2,7 @@ package com.example.data.remote.response
 
 data class DiaryTimeLineResponse(
     val Id: Int,
-    val diaryTimeLineList: ArrayList<TimeLineDiary>,
+    val diaryTimeLineList: List<TimeLineDiary>,
     val isMine: Boolean
 ){
     data class TimeLineDiary(
