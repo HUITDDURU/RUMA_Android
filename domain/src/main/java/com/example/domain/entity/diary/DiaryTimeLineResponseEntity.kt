fb50e490b249a506c176ff6da47f7a78ae243888
unknown <abh0920one@gmail.com>
2022-05-18 22:14:19 +0900

    😎 (#21) - diary api response entity refactor

--- domain/src/main/java/com/example/domain/entity/diary/DiaryTimeLineResponseEntity.kt
+++ domain/src/main/java/com/example/domain/entity/diary/DiaryTimeLineResponseEntity.kt
@@ -2,10 +2,10 @@ package com.example.domain.entity.diary
 
 data class DiaryTimeLineResponseEntity(
     val Id: Int,
-    val diaryTimeLineList: ArrayList<TimeLineDiary>,
+    val diaryTimeLineList: List<TimeLineDiaryEntity>,
     val isMine: Boolean
 ){
-    data class TimeLineDiary(
+    data class TimeLineDiaryEntity(
         val id: Int,
         val title: String,
         val date: String,
--- domain/src/main/java/com/example/domain/entity/diary/GetDiaryListResponseEntity.kt
+++ domain/src/main/java/com/example/domain/entity/diary/GetDiaryListResponseEntity.kt
@@ -1,15 +1,12 @@
 package com.example.domain.entity.diary
 
-data class GetDiaryListResponseEntity(
-    val diaryList : ArrayList<DiaryResponse>
-){
-    data class DiaryResponse(
-        val id: Int,
-        val title: String,
-        val feeling: String,
-        val image: String,
-        val content: String,
-        val date: String,
-        val writer: String
-    )
-}
+data class DiaryResponseEntity(
+    val id: Int,
+    val title: String,
+    val feeling: String,
+    val image: String,
+    val content: String,
+    val date: String,
+    val writer: String
+)
+
