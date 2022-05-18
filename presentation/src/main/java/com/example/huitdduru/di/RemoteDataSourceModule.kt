8f455365a0b23861a6112c3987d28271bbe0ea16
unknown <abh0920one@gmail.com>
2022-05-18 19:11:27 +0900

    🔌 (#21) - diary datasource hilt module add

--- presentation/src/main/java/com/example/huitdduru/di/RemoteDataSourceModule.kt
+++ presentation/src/main/java/com/example/huitdduru/di/RemoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.example.huitdduru.di
 
 import com.example.data.datasource.AuthDataSource
 import com.example.data.datasource.AuthDataSourceImpl
+import com.example.data.datasource.DiaryDataSource
+import com.example.data.datasource.DiaryDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RemoteDataSourceModule {
     abstract fun provideAuthDataSource(
         authDataSourceImpl: AuthDataSourceImpl
     ): AuthDataSource
+
+    @Singleton
+    @Binds
+    abstract fun provideDiaryDataSource(
+        diaryDataSourceImpl: DiaryDataSourceImpl
+    ): DiaryDataSource
 }
