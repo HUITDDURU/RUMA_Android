c6aaaf109d83a11025a3b2598f6d75b1a008653d
unknown <abh0920one@gmail.com>
2022-05-23 23:30:39 +0900

    🔌 (#21) - diary viewmodel get date diary list api

--- presentation/src/main/java/com/example/huitdduru/viewmodel/diary/DiaryViewModel.kt
+++ presentation/src/main/java/com/example/huitdduru/viewmodel/diary/DiaryViewModel.kt
@@ -7,10 +7,12 @@ import com.example.domain.base.BadRequest
 import com.example.domain.base.Forbidden
 import com.example.domain.base.NotFound
 import com.example.domain.base.UnAuthorized
-import com.example.domain.entity.diary.DiaryTimeLineResponseEntity
+import com.example.domain.entity.diary.DateDiaryResponseEntity
 import com.example.domain.entity.diary.GetDiaryListResponseEntity
+import com.example.domain.entity.diary.MonthDiaryResponseEntity
 import com.example.domain.entity.diary.WriteDiaryRequestEntity
-import com.example.domain.usecase.diary.DiaryTimeLineUseCase
+import com.example.domain.usecase.diary.GetDateDiaryUseCase
+import com.example.domain.usecase.diary.GetMonthDiaryUseCase
 import com.example.domain.usecase.diary.GetDiaryListUseCase
 import com.example.domain.usecase.diary.WriteDiaryUseCase
 import com.example.huitdduru.util.MutableEventFlow
@@ -24,7 +26,8 @@ import javax.inject.Inject
 class DiaryViewModel @Inject constructor(
     private val writeDiaryUseCase: WriteDiaryUseCase,
     private val getDiaryListUseCase: GetDiaryListUseCase,
-    private val diaryTimeLineUseCase: DiaryTimeLineUseCase,
+    private val diaryTimeLineUseCase: GetMonthDiaryUseCase,
+    private val getDateDiaryUseCase: GetDateDiaryUseCase,
     private val localDataStorage: LocalDataStorage
 ) : ViewModel() {
 
@@ -91,11 +94,13 @@ class DiaryViewModel @Inject constructor(
         }
     }
 
-    fun timeLine() {
+    fun getMonthDiary(year: Int, month: Int) {
         viewModelScope.launch {
             kotlin.runCatching {
                 diaryTimeLineUseCase.invoke(
-                    localDataStorage.getAccessToken()!!
+                    localDataStorage.getAccessToken()!!,
+                    year,
+                    month
                 )
             }.onSuccess {
                 event(Event.SuccessTimeLine(it))
@@ -109,12 +114,34 @@ class DiaryViewModel @Inject constructor(
                 }
             }
         }
+
+    fun getDateDiary(date: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                getDateDiaryUseCase.invoke(
+                    localDataStorage.getAccessToken()!!,
+                    date
+                )
+            }.onSuccess {
+                event(Event.SuccessGetDateDiary(it))
+            }.onFailure {
+                when(it){
+                    is BadRequest -> event(Event.ErrorMessage("잘못된 요청 형식입니다."))
+                    is UnAuthorized -> event(Event.ErrorMessage("만료된 토큰입니다."))
+                    is Forbidden -> event(Event.ErrorMessage("요청 권한이 없습니다."))
+                    is NotFound -> event(Event.ErrorMessage("잘못된 요청입니다."))
+                }
+            }
+        }
     }
 
     sealed class Event {
         data class SuccessWrite(val status: Boolean = false) : Event()
         data class SuccessGetList(val diaryList : GetDiaryListResponseEntity) : Event()
-        data class SuccessTimeLine(val timeLineList : List<DiaryTimeLineResponseEntity>) : Event()
+        data class SuccessTimeLine(val monthDiary : List<MonthDiaryResponseEntity>) : Event()
+        data class SuccessGetDateDiary(val dateDiary : List<DateDiaryResponseEntity>) : Event()
         data class ErrorMessage(val errorMessage: String) : Event()
     }
 }
+
+
