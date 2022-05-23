8b9ad4f07649683309dd9aeb7bf594ee782ef023
unknown <abh0920one@gmail.com>
2022-05-23 11:21:38 +0900

    😎 (#21) - main activity android entry point add

--- presentation/src/main/java/com/example/huitdduru/view/main/MainActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/main/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.huitdduru.view.main
 
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.example.huitdduru.R
 import com.example.huitdduru.databinding.ActivityMainBinding
 import com.example.huitdduru.base.BaseActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
