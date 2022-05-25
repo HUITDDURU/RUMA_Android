6ac5f1c5835e92ff83286980d4cafd8f31056c6c
unknown <abh0920one@gmail.com>
2022-05-25 15:03:20 +0900

    🔌 (#21) - diary activity layout 구성

--- presentation/src/main/java/com/example/huitdduru/view/home/DiaryActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/home/DiaryActivity.kt
@@ -1,6 +1,7 @@
 package com.example.huitdduru.view.home
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import com.example.huitdduru.R
 import com.example.huitdduru.base.BaseActivity
 import com.example.huitdduru.databinding.ActivityDiaryBinding
@@ -9,6 +10,6 @@ class DiaryActivity : BaseActivity<ActivityDiaryBinding>(R.layout.activity_diary
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
+        binding.contentTv.movementMethod = ScrollingMovementMethod()
     }
 }
