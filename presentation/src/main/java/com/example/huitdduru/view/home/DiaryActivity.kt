97597eba8200419285702c21938a659582d325d2
unknown <abh0920one@gmail.com>
2022-05-25 11:07:06 +0900

    ➕️ (#21) - diary activity

---
+++ presentation/src/main/java/com/example/huitdduru/view/home/DiaryActivity.kt
@@ -0,0 +1,14 @@
+package com.example.huitdduru.view.home
+
+import android.os.Bundle
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseActivity
+import com.example.huitdduru.databinding.ActivityDiaryBinding
+
+class DiaryActivity : BaseActivity<ActivityDiaryBinding>(R.layout.activity_diary) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        
+    }
+}
