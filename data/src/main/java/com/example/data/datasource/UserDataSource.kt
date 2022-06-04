b93657297cc1db2c11e34b5d66bfc8db7def5c8c
unknown <abh0920one@gmail.com>
2022-06-04 13:34:16 +0900

    ➕️ (#31) - user data source interfacce

---
+++ data/src/main/java/com/example/data/datasource/UserDataSource.kt
@@ -0,0 +1,12 @@
+package com.example.data.datasource
+
+import com.example.data.remote.request.EditRequest
+import com.example.data.remote.response.DiaryResponse
+import com.example.data.remote.response.UserInfoResponse
+
+interface UserDataSource {
+    suspend fun edit(header:String, body: EditRequest)
+    suspend fun resign(header: String)
+    suspend fun diaryList(header: String): List<DiaryResponse>
+    suspend fun userInfo(header: String): UserInfoResponse
+}
