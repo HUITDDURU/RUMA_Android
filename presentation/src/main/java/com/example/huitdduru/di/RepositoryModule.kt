3becf4b525dfd790e1dd588b5e339d1290b288d0
unknown <abh0920one@gmail.com>
2022-06-04 18:10:12 +0900

    🔌 (#31) - user repository hilt di module

--- presentation/src/main/java/com/example/huitdduru/di/RepositoryModule.kt
+++ presentation/src/main/java/com/example/huitdduru/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.example.huitdduru.di
 
 import com.example.data.repository.AuthRepositoryImpl
 import com.example.data.repository.DiaryRepositoryImpl
+import com.example.data.repository.UserRepositoryImpl
 import com.example.domain.repository.AuthRepository
 import com.example.domain.repository.DiaryRepository
+import com.example.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +27,10 @@ abstract class RepositoryModule {
     abstract fun provideDiaryRepository(
         diaryRepositoryImpl: DiaryRepositoryImpl
     ) : DiaryRepository
+
+    @Singleton
+    @Binds
+    abstract fun provideUserRepository(
+        userRepositoryImpl: UserRepositoryImpl
+    ) : UserRepository
 }
