8bef70a4c2909c9dc117d6e9f01209d79c16c668
unknown <abh0920one@gmail.com>
2022-06-05 21:26:52 +0900

    🔌 (#31) - my page user info api binding

--- presentation/src/main/java/com/example/huitdduru/view/mypage/MyPageFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/mypage/MyPageFragment.kt
@@ -2,12 +2,33 @@ package com.example.huitdduru.view.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.huitdduru.R
 import com.example.huitdduru.base.BaseFragment
 import com.example.huitdduru.databinding.FragmentMyPageBinding
+import com.example.huitdduru.util.repeatOnStarted
+import com.example.huitdduru.viewmodel.user.UserViewModel
+import com.example.huitdduru.viewmodel.user.UserViewModel.Event
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    private val vm: UserViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fragment = this
+
+        vm.userInfo()
+
+        repeatOnStarted {
+            vm.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: Event) = when(event){
+        is Event.SuccessUserInfo -> { binding.userInfo = event.userInfo }
+        else -> {}
     }
 }
