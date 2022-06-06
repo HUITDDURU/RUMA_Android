b0b32e4fabd372acbc2b7351bbcc3a752df9652c
unknown <abh0920one@gmail.com>
2022-06-06 23:12:01 +0900

    🔌 (#31) - my page recyclerview adapter binding

--- presentation/src/main/java/com/example/huitdduru/view/mypage/MyPageFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/mypage/MyPageFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.huitdduru.R
+import com.example.huitdduru.adapter.MyPageDiaryRecyclerViewAdapter
 import com.example.huitdduru.base.BaseFragment
 import com.example.huitdduru.databinding.FragmentMyPageBinding
 import com.example.huitdduru.util.repeatOnStarted
@@ -14,22 +15,35 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
-class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page),
+    MyPageDiaryRecyclerViewAdapter.OnItemClickListener {
 
     private val vm: UserViewModel by viewModels()
+    private lateinit var adapter: MyPageDiaryRecyclerViewAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
 
-        vm.userInfo()
-
         repeatOnStarted {
             vm.eventFlow.collect { event -> handleEvent(event) }
         }
+        adapter = MyPageDiaryRecyclerViewAdapter(this)
+        binding.diaryRv.adapter = adapter
+
+        vm.userInfo()
+        vm.diaryList()
+    }
+
+    override fun onItemClick(diaryId: Int) {
+        val intent = Intent(requireContext(), DiaryActivity::class.java)
+        intent.putExtra("diaryId", diaryId)
+        startActivity(intent)
     }
 
     private fun handleEvent(event: Event) = when(event){
         is Event.SuccessUserInfo -> { binding.userInfo = event.userInfo }
+        is Event.SuccessGetDiaryList -> { adapter.submitList(event.diaryList) }
         else -> {}
     }
 
