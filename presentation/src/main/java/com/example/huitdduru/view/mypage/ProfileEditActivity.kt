e038cae1ca04d6809c80f2d1b41504d3dbddfa52
unknown <abh0920one@gmail.com>
2022-06-14 08:41:53 +0900

    🔌 (#31) - resign success handle

--- presentation/src/main/java/com/example/huitdduru/view/mypage/ProfileEditActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/mypage/ProfileEditActivity.kt
@@ -1,6 +1,7 @@
 package com.example.huitdduru.view.mypage
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,6 +15,7 @@ import com.example.huitdduru.databinding.ActivityProfileEditBinding
 import com.example.huitdduru.util.ToastType
 import com.example.huitdduru.util.repeatOnStarted
 import com.example.huitdduru.util.uriToMultipart
+import com.example.huitdduru.view.sign.LoginActivity
 import com.example.huitdduru.viewmodel.user.UserViewModel
 import com.example.huitdduru.viewmodel.user.UserViewModel.Event
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,7 +43,17 @@ class ProfileEditActivity :
         is Event.SuccessFileUpload -> { loadImage(binding.profileIv, vm.imageUrl) }
         is Event.SuccessUserInfo -> { binding.userInfo = event.userInfo }
         is Event.SuccessEdit -> { showToast("정보가 수정되었습니다!", ToastType.SUCCESS)}
+        is Event.SuccessResign -> {
+            showToast("탈퇴되었습니다.", ToastType.SUCCESS)
+            moveToLogin()
+        }
         else -> {}
+    }
+
+    private fun moveToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -52,6 +64,7 @@ class ProfileEditActivity :
             .onPositive(
                 text = "탈퇴하기",
                 buttonBackgroundColor = R.drawable.dialog_button_background){
+                vm.resign()
             }
             .show()
     }
