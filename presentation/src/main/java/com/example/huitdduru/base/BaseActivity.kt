9746ac83c00ae0e6de2542cbd62345a8cee4c4e7
unknown <abh0920one@gmail.com>
2022-06-13 13:32:11 +0900

    🔌 (#31) - show awesome dialog function

--- presentation/src/main/java/com/example/huitdduru/base/BaseActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/base/BaseActivity.kt
@@ -1,12 +1,18 @@
 package com.example.huitdduru.base
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.awesomedialog.AwesomeDialog
+import com.example.awesomedialog.body
+import com.example.awesomedialog.icon
+import com.example.awesomedialog.title
+import com.example.huitdduru.R
 import com.example.huitdduru.util.ToastType
 import es.dmoral.toasty.Toasty
 
@@ -27,5 +33,13 @@ abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes private val layoutRes
             ToastType.INFO -> Toasty.info(this, msg, Toast.LENGTH_SHORT, true).show()
             ToastType.WARNING -> Toasty.warning(this, msg, Toast.LENGTH_SHORT, true).show()
         }
+    }
+
+    fun showDialog(title: String, body: String){
+        AwesomeDialog.build(this)
+            .title(title)
+            .body(body)
+            .icon(R.drawable.ic_warning)
+            .show()
     }
 }
