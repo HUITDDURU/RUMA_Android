f3ed904f6aa513c38805653af7eefd7b345d843b
unknown <abh0920one@gmail.com>
2022-06-14 14:16:05 +0900

    🔌 (#33) - match activity put match type & fragment replace

--- presentation/src/main/java/com/example/huitdduru/view/exchange/MatchActivity.kt
+++ presentation/src/main/java/com/example/huitdduru/view/exchange/MatchActivity.kt
@@ -1,6 +1,7 @@
 package com.example.huitdduru.view.exchange
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.huitdduru.R
 import com.example.huitdduru.base.BaseActivity
 import com.example.huitdduru.databinding.ActivityMatchBinding
@@ -10,5 +11,21 @@ class MatchActivity : BaseActivity<ActivityMatchBinding>(R.layout.activity_match
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
         binding.activity = this
+
+        if(intent.hasExtra("type"))
+            matchType(intent.getIntExtra("type", 0))
+    }
+
+    private fun matchType(type: Int) = when(type) {
+        0 -> replace(RandomMateFragment())
+        1 -> replace(LocalMateFragment())
+        else -> {}
+    }
+
+    fun replace(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
