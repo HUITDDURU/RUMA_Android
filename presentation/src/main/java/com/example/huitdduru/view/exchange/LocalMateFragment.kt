7ceeb1fc825849110e48e755778af75434b172e2
unknown <abh0920one@gmail.com>
2022-06-14 10:11:25 +0900

    ➕️ (#33) - 친구 매칭 페이지

---
+++ presentation/src/main/java/com/example/huitdduru/view/exchange/LocalMateFragment.kt
@@ -0,0 +1,16 @@
+package com.example.huitdduru.view.exchange
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.huitdduru.R
+import com.example.huitdduru.base.BaseFragment
+import com.example.huitdduru.databinding.FragmentLocalMateBinding
+
+class LocalMateFragment : BaseFragment<FragmentLocalMateBinding>(R.layout.fragment_local_mate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
