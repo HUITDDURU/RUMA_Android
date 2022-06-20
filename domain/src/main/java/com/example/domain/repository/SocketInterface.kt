bc6af60f7e206cf285d501462e2b9ad76c55e4b9
unknown <abh0920one@gmail.com>
2022-06-20 22:43:44 +0900

    ➕️ (#37) - socket interface

---
+++ domain/src/main/java/com/example/domain/repository/SocketInterface.kt
@@ -0,0 +1,13 @@
+package com.example.domain.repository
+
+import com.example.domain.entity.user.UserInfoResponseEntity
+
+interface SocketInterface {
+    suspend fun connect()
+    suspend fun disconnect()
+    suspend fun matching()
+    suspend fun cancel()
+    suspend fun accept(accept: Boolean)
+    suspend fun localMatching(code: String)
+    suspend fun userInfo(): UserInfoResponseEntity
+}
