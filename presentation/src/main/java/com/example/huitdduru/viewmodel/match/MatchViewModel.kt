6c56473179c60d51346ea8516b892b3e073cae9a
unknown <abh0920one@gmail.com>
2022-06-21 14:13:12 +0900

    ➕️ (#37) - match viewmodel

---
+++ presentation/src/main/java/com/example/huitdduru/viewmodel/match/MatchViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.huitdduru.viewmodel.match
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MatchViewModel : ViewModel() {
+}
