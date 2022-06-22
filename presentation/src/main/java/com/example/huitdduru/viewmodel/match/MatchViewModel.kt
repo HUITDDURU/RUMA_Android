db4250fed638a5e72ac57a5a8d356df2534df606
unknown <abh0920one@gmail.com>
2022-06-22 22:39:52 +0900

    🔌 (#37) - socket connect & disconnect function in match viewmodel

--- presentation/src/main/java/com/example/huitdduru/viewmodel/match/MatchViewModel.kt
+++ presentation/src/main/java/com/example/huitdduru/viewmodel/match/MatchViewModel.kt
@@ -1,8 +1,48 @@
 package com.example.huitdduru.viewmodel.match
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.base.*
+import com.example.domain.usecase.match.*
+import com.example.huitdduru.util.MutableEventFlow
+import com.example.huitdduru.util.asEventFlow
+import com.example.huitdduru.viewmodel.diary.DiaryViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class MatchViewModel : ViewModel() {
+class MatchViewModel @Inject constructor(
+    private val connectSocketUseCase: ConnectSocketUseCase,
+    private val disconnectSocketUseCase: DisconnectSocketUseCase,
+    private val getUserInfoUseCase: GetUserInfoUseCase,
+    private val localMatchingUseCase: LocalMatchingUseCase,
+    private val matchingAcceptUseCase: MatchingAcceptUseCase,
+    private val matchingCancelUseCase: MatchingCancelUseCase,
+    private val matchingUseCase: MatchingUseCase
+): ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    fun connect(){
+        viewModelScope.launch {
+            connectSocketUseCase.invoke()
+        }
+    }
+
+    fun disconnect() {
+        viewModelScope.launch {
+            disconnectSocketUseCase.invoke()
+        }
+    }
+
+    sealed class Event {
+    }
 }
