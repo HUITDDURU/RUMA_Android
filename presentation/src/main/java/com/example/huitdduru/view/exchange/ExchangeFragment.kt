0eb36bd9f3fe5901243fb0c9f9fc8e20354fd6e7
unknown <abh0920one@gmail.com>
2022-06-23 15:23:07 +0900

    🔌 (#37) - socket connect & disconnect call in exchange fragment

--- presentation/src/main/java/com/example/huitdduru/view/exchange/ExchangeFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/exchange/ExchangeFragment.kt
@@ -4,14 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.huitdduru.R
 import com.example.huitdduru.base.BaseFragment
 import com.example.huitdduru.databinding.FragmentExchangeBinding
+import com.example.huitdduru.viewmodel.match.MatchViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(R.layout.fragment_exchange) {
+
+    private val vm: MatchViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
+        vm.connect()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vm.disconnect()
     }
 
     fun moveToMatch(type: Int) {
