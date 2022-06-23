c3dfc70b1fb7f36eba804579cf49de286dc354e6
unknown <abh0920one@gmail.com>
2022-06-23 15:24:26 +0900

    🔌 (#37) - matching & get user info call in random mate fragment

--- presentation/src/main/java/com/example/huitdduru/view/exchange/RandomMateFragment.kt
+++ presentation/src/main/java/com/example/huitdduru/view/exchange/RandomMateFragment.kt
@@ -1,17 +1,23 @@
 package com.example.huitdduru.view.exchange
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.huitdduru.R
 import com.example.huitdduru.base.BaseFragment
 import com.example.huitdduru.databinding.FragmentRandomMateBinding
+import com.example.huitdduru.viewmodel.match.MatchViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RandomMateFragment : BaseFragment<FragmentRandomMateBinding>(R.layout.fragment_random_mate) {
+
+    private val vm: MatchViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fragment = this
+        vm.matching()
+        vm.getUserInfo()
     }
 }
